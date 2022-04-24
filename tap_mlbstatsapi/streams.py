3644ada9bfc4c961167fb623d6c4eeb59be951df
Jon Adelson <gandalfthegraydelson@gmail.com>
2022-04-24 13:27:56 -0400

    Implement schedule stream

--- tap_mlbstatsapi/streams.py
+++ tap_mlbstatsapi/streams.py
@@ -1,62 +1,57 @@
 """Stream type classes for tap-mlbstatsapi."""
 
-from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
 
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_mlbstatsapi.client import MLBStatsAPIStream
 
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
+import statsapi
+
+from time import sleep
 
 
-class UsersStream(MLBStatsAPIStream):
+class ScheduleStream(MLBStatsAPIStream):
     """Define custom stream."""
-    name = "users"
-    path = "/users"
-    primary_keys = ["id"]
-    replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property(
-            "id",
-            th.StringType,
-            description="The user's system ID"
-        ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years"
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address"
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format"
-        ),
-        th.Property("zip", th.StringType),
-    ).to_dict()
 
-
-class GroupsStream(MLBStatsAPIStream):
-    """Define custom stream."""
-    name = "groups"
-    path = "/groups"
-    primary_keys = ["id"]
-    replication_key = "modified"
+    name = "schedule"
+    # primary_keys = ["id"]
+    replication_key = "game_date"
     schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
-    ).to_dict()
+        th.Property("game_id", th.IntegerType),
+        th.Property("game_date", th.StringType),
+        th.Property("game_datetime", th.StringType),
+        th.Property("game_type", th.StringType),
+        th.Property("status", th.StringType),
+        th.Property("away_name", th.StringType),
+        th.Property("home_name", th.StringType),
+        th.Property("away_id", th.IntegerType),
+        th.Property("home_id", th.IntegerType),
+        th.Property("doubleheader", th.StringType),
+        th.Property("game_num", th.IntegerType),
+        th.Property("home_probable_pitcher", th.StringType),
+        th.Property("away_probable_pitcher", th.StringType),
+        th.Property("home_pitcher_note", th.StringType),
+        th.Property("away_pitcher_note", th.StringType),
+        th.Property("away_score", th.IntegerType),
+        th.Property("home_score", th.IntegerType),
+        th.Property("current_inning", th.IntegerType),
+        th.Property("inning_state", th.StringType),
+        th.Property("venue_id", th.IntegerType),
+        th.Property("venue_name", th.StringType),
+        th.Property("winning_team", th.StringType),
+        th.Property("losing_team", th.StringType),
+        th.Property("winning_pitcher", th.StringType),
+        th.Property("losing_pitcher", th.StringType),
+        th.Property("save_pitcher", th.StringType),
+        th.Property("summary", th.StringType),
+        ).to_dict()
+
+    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
+        start_date_default = f"{self.season}-01-01"
+        start_date = self.get_starting_replication_key_value(context) or start_date_default
+        end_date = f"{self.season}-12-31"
+        games = statsapi.schedule(start_date=start_date, end_date=end_date)
+        for game in games:
+            if game["status"] != "Scheduled":
+                yield game
